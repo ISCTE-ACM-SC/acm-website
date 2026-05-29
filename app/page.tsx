01242ffadf426240ce63f56b1f19a86848edc2e1
Valeriooooh <58793169+Valeriooooh@users.noreply.github.com>
2026-05-29 10:31:25 +0100

    fix posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { Header } from "@/components/header"
 import { HeroSection } from "@/components/hero-section"
 import { BlogSection } from "@/components/blog-section"
